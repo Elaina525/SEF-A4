b3bcb4d9903a00818ba9d2c55e6953e5315b740b
Elaina525 <s3880590@student.rmit.edu.au>
2023-10-04 13:24:21 +1100

    add ConditionCheck class

--- src/Artist.java
+++ src/Artist.java
@@ -4,18 +4,18 @@ public class Artist {
     private String ID;
     private String Name;
     private String Address;
-    private String Birthdata;
+    private String Birthdate;
     private String Bio;
     private ArrayList <String> Occupations;
     private ArrayList <String> Genres;
     private ArrayList <String> Awards;
 
-    public Artist(String iD, String name, String address, String birthdata, String bio, ArrayList<String> occupations,
+    public Artist(String iD, String name, String address, String birthdate, String bio, ArrayList<String> occupations,
             ArrayList<String> genres, ArrayList<String> awards) {
         ID = iD;
         Name = name;
         Address = address;
-        Birthdata = birthdata;
+        Birthdate = birthdate;
         Bio = bio;
         Occupations = occupations;
         Genres = genres;
---
+++ src/ConditionCheck.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+
+public class ConditionCheck {
+    
+    public boolean IDCheck(String ID) {
+        
+
+        return true;
+    }
+
+    public boolean birthdateCheck(String Birthdate) {
+
+
+        return true;
+    }
+
+    public boolean addressCheck(String Address) {
+
+
+        return true;
+    }
+
+    public boolean bioCheck(String Bio) {
+
+
+        return true;
+    }
+
+    public boolean occupationsCheck(ArrayList <String> Occupations) {
+
+
+        return true;
+    }
+
+    public boolean awardsCheck(ArrayList <String> Awards) {
+
+        return true;
+    }
+
+    public boolean genresCheck(ArrayList <String> Genres) {
+
+        return true;
+    }
+
+}
